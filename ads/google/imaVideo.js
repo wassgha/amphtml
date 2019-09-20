c398c196fba473c2875c35c9f7f5486ed458609f
Wassim Gharbi <wassgha@gmail.com>
2019-09-20 10:01:44 -0700

    Initial work

--- ads/google/imaVideo.js
+++ ads/google/imaVideo.js
@@ -39,23 +39,17 @@ const PlayerStates = {
  */
 /*eslint-disable*/
 const icons = {
-  'play':
-    `<path d="M8 5v14l11-7z"></path>
+  'play': `<path d="M8 5v14l11-7z"></path>
      <path d="M0 0h24v24H0z" fill="none"></path>`,
-  'pause':
-    `<path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"></path>
+  'pause': `<path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"></path>
      <path d="M0 0h24v24H0z" fill="none"></path>`,
-  'fullscreen':
-    `<path d="M0 0h24v24H0z" fill="none"/>
+  'fullscreen': `<path d="M0 0h24v24H0z" fill="none"/>
      <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>`,
-  'mute':
-    `<path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"></path>
+  'mute': `<path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"></path>
      <path d="M0 0h24v24H0z" fill="none"></path>`,
-  'volume_max':
-    `<path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path>
+  'volume_max': `<path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path>
      <path d="M0 0h24v24H0z" fill="none"></path>`,
-  'seek':
-  `<circle cx="12" cy="12" r="12" />`
+  'seek': `<circle cx="12" cy="12" r="12" />`,
 };
 
 /*eslint-enable */
@@ -248,6 +242,7 @@ export function imaVideo(global, data) {
     'vertical-align': 'middle',
     'text-align': 'center',
     'cursor': 'pointer',
+    'pointer-events': 'auto',
   });
   // Inner div so we can v and h align.
   playButtonDiv = createIcon(global, 'play');
@@ -267,9 +262,10 @@ export function imaVideo(global, data) {
     'bottom': '0px',
     'width': '100%',
     'height': '100px',
-    'background-color': 'rgba(7, 20, 30, .7)',
+    'background-color': 'rgba(0, 0, 0, .7)',
     'background':
-      'linear-gradient(0, rgba(7, 20, 30, .7) 0%, rgba(7, 20, 30, 0) 100%)',
+      'linear-gradient(0, rgba(0, 0, 0, .7) 0%, rgba(0, 0, 0, 0) 100%)',
+    'pointer-events': 'none',
     'box-sizing': 'border-box',
     'padding': '10px',
     'padding-top': '60px',
@@ -295,7 +291,7 @@ export function imaVideo(global, data) {
     'height': '20px',
     'overflow': 'hidden',
     'padding': '5px',
-    'text-shadow': '0px 0px 10px black',
+    'text-shadow': '0px 0px 4px rgba(0,0,0,0.6)',
     'white-space': 'nowrap',
   });
   countdownDiv = global.document.createElement('div');
@@ -310,6 +306,7 @@ export function imaVideo(global, data) {
     'margin-right': '20px',
     'font-size': '1.25em',
     'cursor': 'pointer',
+    'pointer-events': 'auto',
   });
   controlsDiv.appendChild(playPauseDiv);
   // Current time and duration.
@@ -319,7 +316,7 @@ export function imaVideo(global, data) {
     'margin-right': '20px',
     'text-align': 'center',
     'font-size': '14px',
-    'text-shadow': '0px 0px 10px black',
+    'text-shadow': '0px 0px 4px rgba(0,0,0,0.6)',
   });
   timeNode = global.document.createTextNode('-:- / 0:00');
   timeDiv.appendChild(timeNode);
@@ -360,6 +357,7 @@ export function imaVideo(global, data) {
     'top': '50%',
     'margin-top': '-7px',
     'cursor': 'pointer',
+    'pointer-events': 'auto',
   });
   progressMarkerDiv.appendChild(createIcon(global, 'seek'));
   progressBarWrapperDiv.appendChild(progressLine);
@@ -377,6 +375,7 @@ export function imaVideo(global, data) {
     'margin-right': '20px',
     'font-size': '1.25em',
     'cursor': 'pointer',
+    'pointer-events': 'auto',
   });
   controlsDiv.appendChild(muteUnmuteDiv);
 
@@ -392,6 +391,7 @@ export function imaVideo(global, data) {
     'text-align': 'center',
     'font-weight': 'bold',
     'line-height': '1.4em',
+    'pointer-events': 'auto',
   });
   controlsDiv.appendChild(fullscreenDiv);
 
@@ -1326,8 +1326,7 @@ export function showAdControls() {
   [playPauseDiv, timeDiv, progressBarWrapperDiv].forEach(hideElement);
   // set ad control styles
   setStyles(controlsDiv, {
-    'height': miniControls ? '20px' : '30px',
-    'justify-content': 'flex-end',
+    'align-items': 'flex-end',
     'padding': '10px',
   });
   const buttonDefaults = {
